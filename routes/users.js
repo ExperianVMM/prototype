5999beac6a23b52a5034536b6f7335c875e5e236
Rob Harrison <robertharrison224@gmail.com>
2016-01-19 23:12:35 +0000

    added mongo db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,98 @@
 var express = require('express');
+
+var mongoose    =   require("mongoose");
+mongoose.connect('mongodb://localhost:27017/vmm');
+
+var mongoSchema =   mongoose.Schema;
+// create schema
+var userSchema  = {
+    "userEmail" : String,
+    "userPassword" : String
+};
+// create model if not exists.
+var mongoOp = mongoose.model('user',userSchema);
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router
+    .get('/', function(req,res){
+        var response = {};
+        mongoOp.find({},function(err,data){
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : data};
+            }
+            res.json(response);
+        });
+    })
+    .post('/', function(req,res){
+        var db = new mongoOp();
+        var response = {};
+        db.userEmail = req.body.email;
+        db.userPassword = require('crypto').createHash('sha1').update(req.body.password).digest('base64');
+        db.save(function(err){
+            if(err) {
+                response = {"error" : true,"message" : "Error adding data"};
+            } else {
+                response = {"error" : false,"message" : "Data added"};
+            }
+            res.json(response);
+        });
+    });
+
+router
+    .get("/:id", function(req,res){
+        var response = {};
+        mongoOp.findById(req.params.id,function(err,data){
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : data};
+            }
+            res.json(response);
+        });
+    })
+    .put("/:id", function(req,res){
+        var response = {};
+        mongoOp.findById(req.params.id,function(err,data){
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                if(req.body.userEmail !== undefined) {
+                    data.userEmail = req.body.userEmail;
+                }
+                if(req.body.userPassword !== undefined) {
+                    data.userPassword = req.body.userPassword;
+                }
+                data.save(function(err){
+                    if(err) {
+                        response = {"error" : true,"message" : "Error updating data"};
+                    } else {
+                        response = {"error" : false,"message" : "Data is updated for "+req.params.id};
+                    }
+                    res.json(response);
+                })
+            }
+        });
+    })
+    .delete("/:id", function(req,res){
+        var response = {};
+        mongoOp.findById(req.params.id,function(err,data){
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                mongoOp.remove({_id : req.params.id},function(err){
+                    if(err) {
+                        response = {"error" : true,"message" : "Error deleting data"};
+                    } else {
+                        response = {"error" : true,"message" : "Data associated with "+req.params.id+"is deleted"};
+                    }
+                    res.json(response);
+                });
+            }
+        });
+    });
 
 module.exports = router;
